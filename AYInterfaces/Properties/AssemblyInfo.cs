a49483febbe0d620295ecede38b74f861a3ef76e
Jamie Leighton <jamieleighton@bigpond.com>
2015-11-10 12:31:13 +1100

    V1.0.5
    Update for KSP 1.0.5 compatibility

--- AYInterfaces/Properties/AssemblyInfo.cs
+++ AYInterfaces/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
 //[assembly: AssemblyFileVersion("0.12.*")]
-[assembly: KSPAssembly("AYInterfaces", 2, 1)]
+[assembly: KSPAssembly("AYInterfaces", 2, 2)]
