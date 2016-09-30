c6311bf9e9328302299ab0dc52bf9a4e51871225
Jamie Leighton <jamieleighton@bigpond.com>
2016-09-30 20:49:53 +1000

    Squashed 'REPOSoftTechUtils/' changes from 0067b64..a170873
    a170873 Fix Resource request for KSP 1.2
    b5ed105 Fixes for KSP 1.2
    423183c Merge branch 'master' into Dev
    a38b510 Add SI - EC unit conversion routine. Add new status for GenericConvert routine. Add RO mod install routine.
    3e5fbd6 Merge pull request #2 from JPLRepo/Dev
    f055c88 Update ModuleResourceConverter status to include new "Operational" status
    
    git-subtree-dir: REPOSoftTechUtils
    git-subtree-split: a1708739bcd706d4c77cdf092b6c5622c76132d2

--- RSTutilities.cs
+++ RSTutilities.cs
@@ -1,6 +1,5 @@
 ﻿
 
-using HighlightingSystem;
 /**
 * REPOSoftTech KSP Utilities
 * (C) Copyright 2015, Jamie Leighton
@@ -19,6 +18,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Highlighting;
 using UnityEngine;
 using Object = UnityEngine.Object;
 using Random = System.Random;
@@ -153,7 +153,7 @@ namespace RSTUtils
 			Log_Debug("Distance from Home Planet = " + DstFrmHome);
 			return DstFrmHome;
 		}
-
+		
 		public static bool CelestialBodyDistancetoSun(CelestialBody cb, out Vector3d sun_dir, out double sun_dist)
 		{
 			// bodies traced against
@@ -802,24 +802,20 @@ namespace RSTUtils
 		{
 			if (on)
 			{
-				if (part.highlighter == null)
+				if (part.HighlightActive)
 				{
 					var color = XKCDColors.Yellow;
-					var model = part.FindModelTransform("model");
-					part.highlighter = model.gameObject.AddComponent<Highlighter>();
-					part.highlighter.ConstantOn(color);
 					part.SetHighlightColor(color);
 					part.SetHighlight(true, false);
 				}
 			}
 			else
 			{
-				if (part.highlighter != null)
+				if (part.HighlightActive)
 				{
 					part.SetHighlightDefault();
-					part.highlighter.gameObject.DestroyGameObjectImmediate();
-					part.highlighter = null;
-				}
+                    part.SetHighlight(false, false);
+                }
 			}
 		}
 
@@ -837,46 +833,62 @@ namespace RSTUtils
 			return celsius + 273.15f;
 		}
 
-		#endregion Temperature
+        #endregion Temperature
 
-		#region Resources
+        #region Resources
 
-		private static List<PartResource> resources;
-		//Resources
-		public static double GetAvailableResource(Part part, String resourceName)
-		{
-			resources = new List<PartResource>();
-			part.GetConnectedResources(PartResourceLibrary.Instance.GetDefinition(resourceName).id, ResourceFlowMode.ALL_VESSEL, resources);
-			return resources.Sum(pr => pr.amount);
-		}
+        internal static bool requireResource(Vessel craft, string res, double resAmount, bool ConsumeResource, out double resavail)
+        {
+            if (!craft.loaded)
+            {
+                resavail = 0;
+                return false; // Unloaded resource checking is unreliable.
+            }
+            double amount, maxamount;
+            craft.resourcePartSet.GetConnectedResourceTotals(PartResourceLibrary.Instance.GetDefinition(res).id, out amount, out maxamount, true);
+            if (amount < resAmount)
+            {
+                resavail = amount;
+                return false;
+            }
+            if (!ConsumeResource)
+            {
+                resavail = amount;
+                return true;
+            }
+            var amountdrawn = craft.RequestResource(craft.rootPart, PartResourceLibrary.Instance.GetDefinition(res).id, resAmount, true);
+            if (amountdrawn < resAmount * 0.99)
+            {
+                resavail = amountdrawn;
+                return false;
+            }
+            resavail = amountdrawn;
+            return true;
+        }
 
-		public const int MAX_TRANSFER_ATTEMPTS = 4;
-
-		private static double totalReceived;
-		private static double requestAmount;
-		private static double received;
-		public static double RequestResource(Part cvp, String name, double amount)
-		{
-			if (amount <= 0.0)
-				return 0.0;
-			totalReceived = 0.0;
-			requestAmount = amount;
-			for (int attempts = 0; (attempts < MAX_TRANSFER_ATTEMPTS) && (amount > 0.000000000001); attempts++)
-			{
-				received = cvp.RequestResource(name, requestAmount, ResourceFlowMode.ALL_VESSEL);
-				//Log_Debug("requestResource attempt " + attempts);
-				//Log_Debug("requested power = " + requestAmount.ToString("0.0000000000000000000000"));
-				//Log_Debug("received power = " + received.ToString("0.0000000000000000000000"));
-				totalReceived += received;
-				amount -= received;
-				//Log_Debug("amount = " + amount.ToString("0.0000000000000000000000"));
-				if (received <= 0.0)
-					requestAmount = amount * 0.5;
-				else
-					requestAmount = amount;
-			}
-			return totalReceived;
-		}
+        /// <summary>
+        /// Converts Stock EC units to SI units (W,kW,mW)
+        /// </summary>
+        /// <param name="EC">input EC units amount</param>
+        /// <param name="Unit">OUTPUT Unit string</param>
+        /// <returns>converted SI units amount</returns>
+	    public static double ConvertECtoSI(double EC, out string Unit)
+	    {
+            double outputECSI = EC;
+            outputECSI *= 1000; //Watts (W)
+	        Unit = "W";
+	        if (outputECSI > 1000)
+	        {
+	            outputECSI /= 1000; //KiloWatts
+	            Unit = "kW";
+	        }
+	        if (outputECSI > 1000)
+	        {
+                outputECSI /= 1000; //MegaWatts
+                Unit = "mW";
+            }
+	        return outputECSI;
+	    }
 
 		#endregion Resources
 
@@ -1249,7 +1261,9 @@ namespace RSTUtils
 			Inactive,
 			Active,
 			MissingResource,
-			OutputFull
+			OutputFull,
+            ZeroEfficiency
+
 		}
 
 		private static ISRUStatus returnStatus;
@@ -1264,7 +1278,10 @@ namespace RSTUtils
 			if (tmpRegRc.status.ToLower().Contains("inactive")) returnStatus = ISRUStatus.Inactive; //Status is inactive, it's inactive.. Not sure how but sometimes this remains on load even when it's inactive? Hence the test above.
 			if (tmpRegRc.status.ToLower().Contains("missing")) returnStatus = ISRUStatus.MissingResource; //Missing an Input resource makes this appear in the status.
 			if (tmpRegRc.status.ToLower().Contains("full")) returnStatus = ISRUStatus.OutputFull; //If the vessel has nowhere to store the output, full appears in the status.
-			if (tmpRegRc.status.ToLower().Contains("load")) returnStatus = ISRUStatus.Active; //a Percentage Load indicates it is active and actually processing... except when it gets stuck on this.
+            if (tmpRegRc.status.ToLower().Contains("output cap")) returnStatus = ISRUStatus.OutputFull; //If the vessel has nowhere to store the output, output cap: x% appears in the status.
+            if (tmpRegRc.status.ToLower().Contains("load")) returnStatus = ISRUStatus.Active; //a Percentage Load indicates it is active and actually processing... except when it gets stuck on this.
+		    if (tmpRegRc.status.ToLower().Contains("zero efficiency")) returnStatus = ISRUStatus.ZeroEfficiency; //Efficiency has reduced to zero (heat factor?).
+            if (tmpRegRc.status.ToLower().Contains("operational")) returnStatus = ISRUStatus.Active; //a new status with KSP 1.1.3.
 			return returnStatus;
 		}
 
@@ -1314,16 +1331,25 @@ namespace RSTUtils
 			}
 		}
 
-	    internal static bool IsEVEInstalled
-	    {
-	        get
-	        {
-	            return IsModInstalled("EVEManager");
-	            
-	        }
-	    }
+		internal static bool IsEVEInstalled
+		{
+			get
+			{
+				return IsModInstalled("EVEManager");
+				
+			}
+		}
 
-		internal static bool IsOPMInstalled
+        internal static bool IsROInstalled
+        {
+            get
+            {
+                return IsModInstalled("RealismOverhaul");
+
+            }
+        }
+
+        internal static bool IsOPMInstalled
 		{
 			get
 			{
