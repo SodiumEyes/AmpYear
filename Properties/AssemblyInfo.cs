499d36fc6db72febb6d9ad9c7c9d87d2337f1432
JPLRepo <jamieleighton69@gmail.com>
2020-03-08 15:23:16 +1000

    Update for KSP 1.9.x

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.5")]
+[assembly: AssemblyVersion("1.5.7")]
 [assembly: KSPAssembly("AmpYear", 1, 5)] 
